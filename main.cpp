2a28d8cb2ac032af84d933133772ba45c0d3d7fc
DovahChief <pepelimonta@gmail.com>
2017-04-27 17:39:08 -0500

    probando arbol binario generico

--- main.cpp
+++ main.cpp
@@ -3,24 +3,16 @@
 #include "merge.h"
 #include "bintree.h"
 #include <vector>
-
+#include <string>
 
 int main() {
     std::vector<int> v {9,8,7,3,6,4,1,11};
-    bintree arbol;
-            
-    for (auto&& x : v){
-        arbol.agrega_nodo(x, "nodo : "+ std::to_string(x));
-    }
-    arbol.elimina(1); //revisar funcion 
     
-    std::cout<<"recorriendo arbol "<<std::endl;
-    arbol.rec_inorden(arbol.get_raiz());
-    
+	
     std::cout<<"-------ordenando---------"<<std::endl;
     
     mergeSort(v);
-    for(auto&& x : v){
+    for(const auto& x : v){
         std::cout<<x<<std::endl;
     }
     
@@ -31,5 +23,24 @@ int main() {
     std::cout<<"-------buscando---------"<<std::endl;
     std::cout<<((x == true)?"se encontro el ":"no se encontro el ")<<val<<std::endl;
     
+	
+	/*bintree<std::string> arbol;
+            
+    for (const auto& x : v){
+        arbol.agrega_nodo(x, "nodo : "+ std::to_string(x));
+    }
+    
+    arbol.elimina(11); //revisar funcion 
+    
+    std::cout<<"recorriendo arbol "<<std::endl;
+    arbol.rec_inorden(arbol.get_raiz());
+    */
+	bintree<std::string> arbol;
+	arbol.agrega_nodo(2, "nodo : 1");
+	arbol.agrega_nodo(1, "nodo : 2");
+//	arbol.agrega_nodo(3, "nodo : 3");
+	
+//	arbol.rec_inorden(arbol.get_raiz());
+	
     return 0;
 }
