fa14b9debaa2c585d959a575d08070878f8308b9
DovahChief <pepelimonta@gmail.com>
2017-04-26 21:53:37 -0500

    quitando pruebas de debugger

--- main.cpp
+++ main.cpp
@@ -26,11 +26,7 @@ int main() {
     
     int val = 12; 
     auto x = binsearch(v, val);
-   
-    std::cout<<""<<std::endl;
-    int a[] {0,1,2,3,4,5,6};
-    int s[] {0,0,0};
-    llenasub_arr(a, s, 0 , 2);
+
     
     std::cout<<"-------buscando---------"<<std::endl;
     std::cout<<((x == true)?"se encontro el ":"no se encontro el ")<<val<<std::endl;
